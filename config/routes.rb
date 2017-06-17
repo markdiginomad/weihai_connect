852c7ab920bad4a1dd0d22af44db809746a71656
mark antony <markworld223@gmail.com>
2017-06-17 11:57:36 +0000

    modified routes.rb and added the profiles controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :members
+  devise_for :members, :controllers => {:registrations => "users/registrations"}
+  resources :members do 
+    resource :profile
+  end
   resources :contacts
   get '/about' => 'pages#about'
   get '/home' => 'pages#home'
   root 'pages#home'
-    
 end

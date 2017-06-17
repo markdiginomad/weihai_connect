1a1746d2da4c3f4c554b0bf0a976ff0f499efbb1
mark antony <markworld223@gmail.com>
2017-06-17 12:04:08 +0000

    modified controller and added profile views

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,3 +1,9 @@
 class ProfilesController < ApplicationController
+    def new
+    @member = Member.find( params[user_id] )
+    @profile =@member.build.profile
+    end
     
+    def edit 
+    end 
 end 
